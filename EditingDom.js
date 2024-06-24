e3fbc2f693cf996529e5212072416a447b5cf1ec
RPana97 <panagrosso.ryan@gmail.com>
2024-06-24 13:46:11 -0400

    Update EditingDom.js

--- EditingDom.js
+++ EditingDom.js
@@ -1,30 +1,30 @@
-let listItems = document.querySelectorAll('#list li');
-listItems.forEach(item => {
+let listItems = document.querySelectorAll('#list li'); //selects all li elements in the id list
+listItems.forEach(item => {                            //loops through each list item to change apples to granny smith
     if (item.textContent === 'Apples'){
         item.textContent = 'Granny Smith Apples';
     }
 });
-listItems.forEach (item =>{
+listItems.forEach (item =>{                            //loops through each list to remove oat milk
     if (item.textContent === 'Oat Milk'){
         item.remove();
     }
 });
 
-let Kombucha = document.createElement('li');
+let Kombucha = document.createElement('li');           //creates new li
 Kombucha.innerHTML = "Kombucha";
-let list = document.querySelector ('#list');
+let list = document.querySelector ('#list');           //appends kombucha to list
 list.appendChild (Kombucha);
 
-list.textContent = ('');
+list.textContent = ('');                               //clears the list contents
 
-let newItemlist = ['protein bars', 'almonds', 'peanut butter'];
+let newItemlist = ['protein bars', 'almonds', 'peanut butter'];  //creates an array
 newItemlist.forEach (item =>{
-    let listed = document.createElement('li');
+    let listed = document.createElement('li');         //creates a new li element for each item
     listed.textContent = item; 
     list.appendChild (listed);
 });
 
-Array.from (document.querySelectorAll('#list li')).forEach(item =>{
+Array.from (document.querySelectorAll('#list li')).forEach(item =>{ //converts items to an array then loops through each and adds important class to almonds
 if (item.textContent === 'almonds') {
     item.classList.add ('important');
 }
